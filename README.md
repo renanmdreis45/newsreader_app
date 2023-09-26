5d43bb58ff6677ce4b76e7ba1c112a0fb6fc1115
Renan <renanmdreis@alu.ufc.br>
2023-09-26 17:22:59 -0300

    finish news info and add news model

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# newsreader_app
+# News Reader App
 
-A new Flutter project.
+Repository for a news reader application.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Flutter and Dart were chosen as the main technologies for this project.
+## Architectural Decisions
+The file structure is divided into 4 main parts:
+- Models
+- Repository
+- Block/Provider
+- UI
--- lib/widgets/newsheadline.dart
+++ lib/widgets/newsheadline.dart
@@ -2,66 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:newsreader_app/utils/utils.dart';
 
 class NewsHeadline extends StatelessWidget {
+  const NewsHeadline({super.key});
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.only(left: 10, right: 5, bottom: 10),
+      padding: const EdgeInsets.only(left: 10, right: 5, bottom: 10),
       child: Card(
         elevation: 5,
         child: Row(children: [
           Image(
             width: MediaQuery.of(context).size.width * 0.33,
             height: 140,
-            image: NetworkImage(
-                "https://www.google.com/imgres?imgurl=https%3A%2F%2Fplay-lh.googleusercontent.com%2FOABfRfKxyvf6PUg_6YBJXvbKSbegFsBKvFDEm5jOA0rC5k1la-OwmnWz6GK55vr3EGA&tbnid=EePz8BH2gqhwTM&vet=12ahUKEwjekb319r6BAxVGNbkGHfiXBsIQMygAegQIARB1..i&imgrefurl=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.home.news.breaking%26hl%3Dpt&docid=goVjQeX4cPp1xM&w=512&h=512&q=news&ved=2ahUKEwjekb319r6BAxVGNbkGHfiXBsIQMygAegQIARB1"),
+            image: const NetworkImage(
+                "https://previews.123rf.com/images/alhovik/alhovik1709/alhovik170900031/86481591-breaking-news-background-world-global-tv-news-banner-design.jpg"),
             fit: BoxFit.fill,
           ),
-          SizedBox(
+          const SizedBox(
             width: 10,
           ),
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text("Lorem ipsum",
-                style: textStyle(16, Colors.black, FontWeight.w600)),
-            SizedBox(
-              height: 3,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("John Smith",
-                    style: textStyle(14, Colors.grey, FontWeight.bold)),
-                Text("12-05-21",
-                    style: textStyle(14, Colors.grey, FontWeight.bold))
-              ],
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.play_arrow_sharp,
-                  size: 43,
-                  color: Colors.yellow,
-                ),
-                Icon(Icons.stop, size: 43, color: Colors.black),
-                Container(
-                    width: 70,
-                    height: 32,
-                    child: TextButton(
-                      onPressed: () {},
-                      style:
-                          TextButton.styleFrom(backgroundColor: Colors.yellow),
-                      child: Text("View",
-                          style: textStyle(15, Colors.black, FontWeight.bold)),
-                    ))
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            )
-          ])
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  Text("Software is a companys third-largest expense, but because its spread across multiple departments, its the CFOs responsibility to find places to cut.",
+                      style: textStyle(16, Colors.black, FontWeight.w600)),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("John Smith",
+                            style: textStyle(14, Colors.grey, FontWeight.bold)),
+                        Text("12-05-21",
+                            style: textStyle(14, Colors.grey, FontWeight.bold))
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.play_arrow_sharp,
+                              size: 43,
+                              color: Colors.yellow,
+                            ),
+                            Icon(Icons.stop, size: 43, color: Colors.black),
+                          ],
+                        ),
+                        Container(
+                            width: 70,
+                            height: 32,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.yellow),
+                              child: Text("View",
+                                  style:
+                                      textStyle(15, Colors.black, FontWeight.bold)),
+                            ))
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  )
+                ]),
+              ))
         ]),
       ),
     );
