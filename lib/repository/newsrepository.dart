dcb84c727bc9ce61edc3a7cc363e225c48cf3602
Renan <renanmdreis@alu.ufc.br>
2023-09-26 19:07:23 -0300

    add Url

---
+++ lib/repository/newsrepository.dart
@@ -0,0 +1,6 @@
+class NewsRepository {
+  getNews(String category, String country) {
+    String url =
+        "https://newsapi.org/v2/everything?q=Apple&from=2023-09-26&sortBy=popularity&apiKey=8b6390f9c43341ee95bf885149dd5a84";
+  }
+}
